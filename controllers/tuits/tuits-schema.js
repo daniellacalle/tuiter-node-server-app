64de2dec2f3ce77d1e5f990940aa4aeadfc3f28f
Daniella Calle <daniella.calle@gmail.com>
2023-04-09 19:54:03 -0400

    a9 seemingly done

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+// need to put new before mongoose?
+const schema = mongoose.Schema({   tuit: String,
+                                   likes: Number,
+                                   liked: Boolean,
+                               },
+                               {collection: 'tuits'});
+export default schema;
